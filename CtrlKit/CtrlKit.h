9e4a7c3b1fa00b8694c9dbb3de164f9ba99777df
ExoCore-Kernel <jacktp2013@gmail.com>
2025-07-19 14:35:31 +0800

    Ignore theos vendor directory

---
+++ CtrlKit/CtrlKit.h
@@ -0,0 +1,3 @@
+// Umbrella header for CtrlKit.
+// Add import lines for each public header, like this: #import <CtrlKit/XXXAwesomeClass.h>
+// Don’t forget to also add them to CtrlKit_PUBLIC_HEADERS in your Makefile!
